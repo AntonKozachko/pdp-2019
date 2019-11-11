33e51987b4ce934a68d0609a1e0f5ad0f258ea44
KYIV\antkoz <antkoz@ciklum.com>
2019-11-11 12:05:38 +0200

    SJ-182: fix errors

--- auth-service/src/middlewares/verify-token.js
+++ auth-service/src/middlewares/verify-token.js
@@ -5,34 +5,33 @@ import config from '../config.json';
 import logger from '../libs/logger';
 import { User } from '../users/models/user.model';
 
-const log = logger.get('Id_By_Token_Middleware');
+const log = logger.get('Parse_Token_Middleware');
 
-export class ParseTokenMiddleware {
-  static verify (req, res, next) {
-    const { headers: { authorization = '' } } = req;
+export function parseToken (req, res, next) {
+  const { headers: { authorization = '' } } = req;
 
-    const token = authorization.replace('Bearer ', '');
+  const token = authorization.replace('Bearer ', '');
 
-    if (isEmpty(token)) {
-      return next();
-    }
-
-    const isBlacklistedToken = User.isBlacklistedToken(token);
-    if (isBlacklistedToken) {
-      log.error('Token is blacklisted');
-
-      return next();
-    }
-
-    return jwt.verify(token, config.secret, (err, decoded) => {
-      if (err) {
-        log.error(err);
-        return next();
-      }
-
-      req.userId = decoded.sub;
-
-      return next();
-    });
+  if (isEmpty(token)) {
+    log.info('Token is not provided');
+    return next();
   }
+
+  const isBlacklistedToken = User.isBlacklisted(token);
+  if (isBlacklistedToken) {
+    log.error('Token is blacklisted');
+
+    return next();
+  }
+
+  return jwt.verify(token, config.secret, (err, decoded) => {
+    if (err) {
+      log.error(err);
+      return next();
+    }
+    log.info(`Request from userId=${decoded.sub}`);
+    req.userId = decoded.sub;
+
+    return next();
+  });
 }
--- auth-service/src/users/index.js
+++ auth-service/src/users/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { ParseTokenMiddleware } from '../middlewares/verify-token';
+import { parseToken } from '../middlewares/verify-token';
 import {
   VerifyTokenHandler,
   RegisterHandler,
@@ -22,9 +22,9 @@ const deleteHandler = new DeleteHandler();
 // routes
 router.post('/authenticate', (req, res, next) => authenticateHandler.execute(req, res, next));
 router.post('/verify', (req, res, next) => verifyTokenHandler.execute(req, res, next));
-router.get('/all', ParseTokenMiddleware.verify, (req, res, next) => getUsersHandler.execute(req, res, next));
-router.put('/', ParseTokenMiddleware.verify, (req, res, next) => updateHandler.execute(req, res, next));
+router.get('/all', parseToken, (req, res, next) => getUsersHandler.execute(req, res, next));
+router.put('/', parseToken, (req, res, next) => updateHandler.execute(req, res, next));
 router.post('/', (req, res, next) => registerHandler.execute(req, res, next));
-router.delete('/', ParseTokenMiddleware.verify, (req, res, next) => deleteHandler.execute(req, res, next));
+router.delete('/', parseToken, (req, res, next) => deleteHandler.execute(req, res, next));
 
 export const userRouter = router;
