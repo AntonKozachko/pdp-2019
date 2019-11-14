b0d1815c47737edc3c0de029f93b0b2ab9aead29
KYIV\antkoz <antkoz@ciklum.com>
2019-11-14 17:21:18 +0200

    SJ-187: fix scripts

--- devops/scripts/check-affected-projects.sh
+++ devops/scripts/check-affected-projects.sh
@@ -5,8 +5,6 @@ CHANGED_FILES=($(git diff --name-only $TRAVIS_COMMIT_RANGE))
 echo "Changed files:"
 
 declare -a newArray=()
-declare CI_BUILD_AUTH="BUILD"
-declare CO_BUILD_WEB="BUILD"
 
 # iterate over urls and get first level folder
 for folder in "${CHANGED_FILES[@]}"
@@ -22,26 +20,29 @@ done
 # extract affected projects
 affectedProjects=($(echo "${newArray[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
 
-for folder in "${affectedProjects[@]}"
-do
-  # check if changes were in auth-service
-  if [ $folder == "auth-service" ]
-  then
-    CI_BUILD_AUTH="BUILD"
-  else
-    CI_BUILD_AUTH="SKIP"
-  fi
-  # check if changes were in web-app
-  if [ $folder == "web-app" ]
-  then
-    CI_BUILD_WEB="BUILD"
-  else
-    CI_BUILD_WEB="SKIP"
-  fi
-done
+containsElement () {
+  local e match="$1"
+  shift
+  for e; do [[ "$e" == "$match" ]] && return 1; done
+  return 0
+}
 
-echo "AUTH: $CI_BUILD_AUTH"
-echo "WEB-APP: $CI_BUILD_WEB"
+containsElement "auth-service" "${affectedProjects[@]}"
+if [ $? -eq 1 ]; then
+  CI_BUILD_AUTH="BUILD"
+else
+  CI_BUILD_AUTH="SKIP"  
+fi
+
+containsElement "web-app" "${affectedProjects[@]}"
+if [ $? == 1 ]; then
+  CI_BUILD_WEB="BUILD"
+else
+  CI_BUILD_WEB="SKIP"  
+fi
+
+echo "AUTH: ${CI_BUILD_AUTH}"
+echo "WEB: ${CI_BUILD_WEB}"
 
 export CI_BUILD_AUTH
 export CI_BUILD_WEB
--- devops/scripts/install-auth-svc.sh
+++ devops/scripts/install-auth-svc.sh
@@ -1,11 +1,12 @@
 echo "Install auth service"
 
-source ./devops/scripts/check-affected-projects.sh
-
-echo "AUTH CHANGED: $CI_BUILD_AUTH"
-echo "WEB CHANGED: $CI_BUILD_WEB"
-
 curDir=$PWD
 
-cd $curDir/auth-service
-npm install
+source ./devops/scripts/check-affected-projects.sh
+
+if [ "$CI_BUILD_AUTH" == "BUILD" ]; then
+  cd $curDir/auth-service
+  npm install
+else
+  echo "Skip auth due to absence of changes"
+fi
