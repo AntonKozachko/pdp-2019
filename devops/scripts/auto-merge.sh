8efde11dfbe67294ed09dd0bdfb6e0e837696936
Anton Kozachko <anton_ko@hotmail.com>
2019-11-14 22:29:50 +0200

    revert changes

--- devops/scripts/auto-merge.sh
+++ devops/scripts/auto-merge.sh
@@ -1,22 +1,14 @@
 export GIT_COMMITTER_EMAIL="travis@travis"
 export GIT_COMMITTER_NAME="Travis CI"
 
-BRANCH_TO_MERGE_INTO="master"
-GITHUB_REPO="AntonKozachko/pdp-2019.git"
+git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
 
-# Since Travis does a partial checkout, we need to get the whole thing
-repo_temp=$(mktemp -d)
-git clone "https://github.com/$GITHUB_REPO" "$repo_temp"
+git fetch --all || exit
 
-# shellcheck disable=SC2164
-cd "$repo_temp"
+git checkout master || exit
 
-printf 'Checking out %s\n' "$BRANCH_TO_MERGE_INTO" >&2
-git checkout "$BRANCH_TO_MERGE_INTO"
+echo "Merging..."
+git merge --no-ff "$TRAVIS_COMMIT" || exit
 
-printf 'Merging %s\n' "$TRAVIS_COMMIT" >&2
-git merge --ff-only "$TRAVIS_COMMIT"
-
-printf 'Pushing to %s\n' "$GITHUB_REPO" >&2
-
+echo "Pushing to repo..."
 git push "https://$GITHUB_SECRET_TOKEN@github.com/AntonKozachko/pdp-2019.git"
