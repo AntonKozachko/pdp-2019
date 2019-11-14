df20c5dc9e0c82128720e686895eb794240bc5c7
KYIV\antkoz <antkoz@ciklum.com>
2019-11-14 11:33:00 +0200

    SJ-183: automerge

---
+++ devops/scripts/auto-merge.sh
@@ -0,0 +1,12 @@
+export GIT_COMMITTER_EMAIL="anton_ko@hotmail.com"
+export GIT_COMMITTER_NAME="AntonKozachko"
+
+git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
+
+git fetch --all || exit
+
+git checkout master || exit
+
+git merge --no-ff "$TRAVIS_COMMIT" || exit
+
+git push @github.com/">https://${GITHUB_SECRET_TOKEN}@github.com/AntonKozachko/pdp-2019.git" || exit
