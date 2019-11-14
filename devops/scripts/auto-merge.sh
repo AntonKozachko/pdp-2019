77c6be92ded55e617d67bbc3326473512dca06a8
Anton Kozachko <anton_ko@hotmail.com>
2019-11-14 22:08:57 +0200

    fix repo vars

--- devops/scripts/auto-merge.sh
+++ devops/scripts/auto-merge.sh
@@ -1,3 +1,3 @@
 curl -o /tmp/travis-automerge https://raw.githubusercontent.com/cdown/travis-automerge/master/travis-automerge
 chmod a+x /tmp/travis-automerge
-BRANCHES_TO_MERGE_REGEX='^feature/' BRANCH_TO_MERGE_INTO=develop GITHUB_REPO=cdown/srt /tmp/travis-automerge
+BRANCHES_TO_MERGE_REGEX='^feature/' BRANCH_TO_MERGE_INTO=master GITHUB_REPO=AntonKozachko/pdp-2019 /tmp/travis-automerge
