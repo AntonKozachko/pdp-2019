fd4c6a377b3ad40a22ef43542a69791855f83825
KYIV\antkoz <antkoz@ciklum.com>
2019-11-14 13:24:39 +0200

    SJ-183: commit vars

--- devops/scripts/auto-merge.sh
+++ devops/scripts/auto-merge.sh
@@ -1,5 +1,5 @@
-export GIT_COMMITTER_EMAIL="anton_ko@hotmail.com"
-export GIT_COMMITTER_NAME="AntonKozachko"
+export GIT_COMMITTER_EMAIL="travis@travis"
+export GIT_COMMITTER_NAME="Travis CI"
 
 git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
 
@@ -9,4 +9,4 @@ git checkout master || exit
 
 git merge --no-ff "$TRAVIS_COMMIT" || exit
 
-git push "@github.com/https://${GITHUB_SECRET_TOKEN}@github.com/AntonKozachko/pdp-2019.git"
+git push "https://$GITHUB_SECRET_TOKEN@github.com/AntonKozachko/pdp-2019.git"
