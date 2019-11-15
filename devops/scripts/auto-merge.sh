01aca32fbaca2b34920dcd3ce4e8af4bd7be7680
KYIV\antkoz <antkoz@ciklum.com>
2019-11-15 11:17:02 +0200

    test 5

--- devops/scripts/auto-merge.sh
+++ devops/scripts/auto-merge.sh
@@ -1,17 +1,6 @@
 export GIT_COMMITTER_EMAIL="travis@travis"
 export GIT_COMMITTER_NAME="Travis CI"
 
-echo "Continuous integration: $CONTINUOUS_INTEGRATION"
-echo "Travis branch: $TRAVIS_BRANCH"
-echo "Travis event type: $TRAVIS_EVENT_TYPE"
-echo "Travis job id: $TRAVIS_JOB_ID"
-echo "Travis job name: $TRAVIS_JOB_NAME"
-echo "Travis pull request: $TRAVIS_PULL_REQUEST"
-echo "Travis pull request branch: $TRAVIS_PULL_REQUEST_BRANCH"
-echo "Travis tag: $TRAVIS_TAG"
-echo "Travis test result: $TRAVIS_TEST_RESULT"
-
-exit 0
 git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
 
 git fetch --all || exit
