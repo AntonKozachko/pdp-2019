6f6dd2f0e18997ede6edaea0b62cbd009aced1e4
KYIV\antkoz <antkoz@ciklum.com>
2019-11-15 13:51:38 +0200

    config

--- devops/scripts/auto-merge.sh
+++ devops/scripts/auto-merge.sh
@@ -2,10 +2,8 @@ export GIT_COMMITTER_EMAIL="travis@travis"
 export GIT_COMMITTER_NAME="Travis CI"
 
 git config --global credential.helper cache
-git config --global user.password "$GITHUB_SECRET_TOKEN"
-git config --global user.name "anton_ko@hotmail.com"
 git config --global committer.name "$GIT_COMMITTER_NAME"
-git config --global committer.name "$GIT_COMMITTER_EMAIL"
+git config --global committer.email "$GIT_COMMITTER_EMAIL"
 
 git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
 
@@ -15,4 +13,4 @@ git checkout master || exit
 
 git merge --no-ff "$TRAVIS_COMMIT" || exit
 
-git push "https://github.com/AntonKozachko/pdp-2019.git"
+git push "https://anton_ko@hotmail.com:$GITHUB_SECRET_TOKEN@github.com/AntonKozachko/pdp-2019.git"
