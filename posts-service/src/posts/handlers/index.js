9f3bca5c5409446da430e27e7bf2b99abb83b1f7
Anton <anton_ko@hotmail.com>
2019-11-19 22:20:52 +0200

    SJ-192: get all posts (#27)
    * SJ-192: get all posts

---
+++ posts-service/src/posts/handlers/index.js
@@ -0,0 +1,1 @@
+export { GestPostsHandler } from './get-all';
--- posts-service/src/posts/index.js
+++ posts-service/src/posts/index.js
@@ -1,10 +1,10 @@
 import express from 'express';
 
-import { verifyUser } from '../middlewares/verify-user';
+import { GestPostsHandler } from './handlers';
 
 const router = express.Router();
 
 // routes
-router.get('', verifyUser, (req, res) => res.json('OK'));
+router.get('', (req, res, next) => new GestPostsHandler().execute(req, res, next));
 
 export const postsRouter = router;
