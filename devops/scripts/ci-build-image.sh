6230bfc9b29e2917a76f29247038a822b7388ba1
Anton <anton_ko@hotmail.com>
2019-12-10 23:00:17 +0200

    rework build script (#36)

--- devops/scripts/ci-build-image.sh
+++ devops/scripts/ci-build-image.sh
@@ -26,10 +26,16 @@ if [ $TRAVIS_EVENT_TYPE == "push" ] && [ $TRAVIS_BRANCH == "master" ]; then
     docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" || exit 1
 
     echo "Start building $target image"
-    docker build -t $DOCKER_REGISTRY/$DOCKER_REPO_NAME:$TRAVIS_BUILD_NUMBER .
+    docker build -t $DOCKER_REPO_NAME:latest .
+
+    docker tag "$DOCKER_REPO_NAME:latest" "$DOCKER_REGISTRY/$DOCKER_REPO_NAME:$TRAVIS_BUILD_NUMBER" || exit 1
+    docker tag "$DOCKER_REPO_NAME:latest" "$DOCKER_REGISTRY/$DOCKER_REPO_NAME:latest" || exit 1
 
     echo "Try to publish $target image to $DOCKER_REGISTRY/$DOCKER_REPO_NAME"
-    docker push $DOCKER_REGISTRY/$DOCKER_REPO_NAME:$TRAVIS_BUILD_NUMBER || exit 1
+
+    docker push "$DOCKER_REGISTRY/$DOCKER_REPO_NAME:$TRAVIS_BUILD_NUMBER" || exit 1
+    docker push "$DOCKER_REGISTRY/$DOCKER_REPO_NAME:latest" || exit 1
+
     echo "$(tput setaf 2)Success$(tput sgr 0)"
 else
     echo "$(tput setaf 3)Skip build $target$(tput sgr 0)"
