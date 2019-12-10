55702af1c4d16c701049c2f3cb6eccf464d5aa51
KYIV\antkoz <antkoz@ciklum.com>
2019-12-10 10:14:32 +0200

    SJ-195: fix vars

--- devops/scripts/check-affected-projects.sh
+++ devops/scripts/check-affected-projects.sh
@@ -1,6 +1,4 @@
-target=$1
-
-echo "Check target $target ..."
+echo "Check target $1 ..."
 
 CHANGED_FILES=($(git diff --name-only $TRAVIS_COMMIT_RANGE))
 
@@ -11,7 +9,7 @@ containsElement () {
   return 0
 }
 
-containsElement $target "${CHANGED_FILES[@]}"
+containsElement $1 "${CHANGED_FILES[@]}"
 if [ $? -eq 1 ]; then
   SHOULD_BUILD="1"
 else
