71c249bed9c11f1e0e7bfcc1ecf100003e48d586
KYIV\antkoz <antkoz@ciklum.com>
2019-12-10 13:57:11 +0200

    fix set env

--- devops/scripts/get-docker-repo-name.sh
+++ devops/scripts/get-docker-repo-name.sh
@@ -1,13 +1,13 @@
 target=$1
 
 if [ "$target" == "web-app" ]; then
-  DOCKER_REPO_NAME = "pdp-web-app"
+  DOCKER_REPO_NAME="pdp-web-app"
 elif [ "$target" == "auth-service" ]; then
-  DOCKER_REPO_NAME = "pdp-auth-svc"
+  DOCKER_REPO_NAME="pdp-auth-svc"
 elif [ "$target" == "posts-service" ]; then
-  DOCKER_REPO_NAME = "pdp-posts-svc"
+  DOCKER_REPO_NAME="pdp-posts-svc"
 else
-  DOCKER_REPO_NAME = ""
+  DOCKER_REPO_NAME=""
 fi
 
 echo "FOUND REPO: $DOCKER_REPO_NAME"
