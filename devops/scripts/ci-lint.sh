91e78113e302c206841ef3758ff611b7f2391039
KYIV\antkoz <antkoz@ciklum.com>
2019-12-10 12:11:19 +0200

    SJ-195: synchronous steps

--- devops/scripts/ci-lint.sh
+++ devops/scripts/ci-lint.sh
@@ -3,10 +3,10 @@ curDir=$PWD
 
 source ./devops/scripts/check-affected-projects.sh $target
 
-if [ "$SHOULD_BUILD" == "1" ]; then
+if [ "$SHOULD_BUILD" == "1" ] && [ $TRAVIS_PULL_REQUEST == 1 ]; then
   echo "$(tput setaf 2)Lint $target$(tput sgr 0)"
   cd $curDir/$target
-  npm install
+  # npm install
   npm run lint
 else
   echo "$(tput setaf 3)Skip $target lint due to absence of changes$(tput sgr 0)"
--- devops/scripts/ci-test.sh
+++ devops/scripts/ci-test.sh
@@ -3,10 +3,10 @@ curDir=$PWD
 
 source ./devops/scripts/check-affected-projects.sh $target
 
-if [ "$SHOULD_BUILD" == "1" ]; then
+if [ "$SHOULD_BUILD" == "1" ] && [ $TRAVIS_PULL_REQUEST == 1 ]; then
   echo "$(tput setaf 2)Test $target$(tput sgr 0)"
   cd $curDir/$target
-  npm install
+  # npm install
   npm run test
 else
   echo "$(tput setaf 3)Skip $target test due to absence of changes$(tput sgr 0)"
