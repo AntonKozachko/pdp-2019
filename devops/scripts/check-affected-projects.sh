1383e1ab59a9c413cbbd0293bf87871c82817abc
KYIV\antkoz <antkoz@ciklum.com>
2019-12-10 10:41:00 +0200

    SJ-195: check project script

--- devops/scripts/check-affected-projects.sh
+++ devops/scripts/check-affected-projects.sh
@@ -5,10 +5,8 @@ echo "Check target $target ..."
 git diff --name-only $TRAVIS_COMMIT_RANGE | sort -u | uniq | grep $target > /dev/null
 if [ $? -eq 0 ]; then
   SHOULD_BUILD="1"
-  echo "Build needed"
 else
   SHOULD_BUILD="0"
-  echo "Skip build"
 fi
 
 export SHOULD_BUILD
