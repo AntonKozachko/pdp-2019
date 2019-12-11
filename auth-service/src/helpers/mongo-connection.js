406c8748ad2ba2ae35a47776ec19cc1ea18cf1fc
Anton <anton_ko@hotmail.com>
2019-12-11 23:40:26 +0200

    revert changes (#47)

--- auth-service/src/helpers/mongo-connection.js
+++ auth-service/src/helpers/mongo-connection.js
@@ -7,9 +7,7 @@ const log = logger.get('MONGO-USERS', { ignoreLogLevel: true });
 
 const mongoPort = process.env.MONGO_PORT;
 const mongoHost = process.env.MONGO_HOST;
-const mongoProtocol = process.env.NODE_ENV === 'production' ? 'http' : 'mongodb';
-
-export const connection = `${mongoProtocol}://${mongoHost}:${mongoPort}/users`;
+export const connection = `mongodb://${mongoHost}:${mongoPort}/users`;
 
 const initializeUserCollection = () => User.createCollection()
   .then(() => log.info('User collection initialized'))
