8597d426f20c1feefeb0d28831b8c4c6665ca546
KYIV\antkoz <antkoz@ciklum.com>
2019-12-11 11:59:38 +0200

    deploy scripts

---
+++ devops/scripts/ci-deploy-ecs.sh
@@ -0,0 +1,18 @@
+target=$TARGET
+AWS_REGION="eu-central-1"
+DOCKER_URL="docker.io/antkozdocker"
+PDP_SERVICE="pdp-service"
+PDP_CLUSTER_NAME="pdp-cluster"
+
+if [ $TRAVIS_EVENT_TYPE == "push" ] && [ $TRAVIS_BRANCH == "master" ]; then
+  echo "Start deploy proccess"
+  
+  pip install --user awscli
+  export PATH=$PATH:$HOME/.local/bin
+
+  source ./devops/scripts/get-docker-repo-name.sh $target
+
+  ./devops/scripts/ecs.sh -c $PDP_CLUSTER_NAME -n $PDP_SERVICE -i "$DOCKER_URL/$DOCKER_REPO_NAME:latest" -r $AWS_REGION -t 240
+else
+  echo "Skip deploy"
+fi
