36c9ec2d5fe9e20fe39833aed5377b7b1a06a8d0
KYIV\antkoz <antkoz@ciklum.com>
2019-12-11 12:54:31 +0200

    task definition

--- devops/scripts/ci-deploy-ecs.sh
+++ devops/scripts/ci-deploy-ecs.sh
@@ -1,7 +1,6 @@
 target=$TARGET
 AWS_REGION="eu-central-1"
-DOCKER_URL="docker.io/antkozdocker"
-PDP_SERVICE="pdp-service"
+DOCKER_REGISTRY_URL="docker.io/$DOCKER_REGISTRY"
 PDP_CLUSTER_NAME="pdp-cluster"
 
 if [ $TRAVIS_EVENT_TYPE == "push" ] && [ $TRAVIS_BRANCH == "master" ]; then
@@ -13,7 +12,7 @@ if [ $TRAVIS_EVENT_TYPE == "push" ] && [ $TRAVIS_BRANCH == "master" ]; then
   source ./devops/scripts/get-docker-repo-name.sh $target
   TASK_NAME=$DOCKER_REPO_NAME
 
-  ./devops/scripts/ecs.sh -c $PDP_CLUSTER_NAME -n $PDP_SERVICE -d $TASK_NAME -i "$DOCKER_URL/$DOCKER_REPO_NAME:latest" -r $AWS_REGION -t 240
+  ./devops/scripts/ecs.sh -c $PDP_CLUSTER_NAME -d $TASK_NAME -i "$DOCKER_REGISTRY_URL/$DOCKER_REPO_NAME:latest" -r $AWS_REGION -t 240
 else
   echo "Skip deploy"
 fi
