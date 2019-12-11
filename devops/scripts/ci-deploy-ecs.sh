8a3e7cd9ada3f7ff9cc1d4c814ba91133559b5c2
Anton <anton_ko@hotmail.com>
2019-12-11 14:04:54 +0200

    service instead of task (#42)

--- devops/scripts/ci-deploy-ecs.sh
+++ devops/scripts/ci-deploy-ecs.sh
@@ -4,15 +4,15 @@ DOCKER_REGISTRY_URL="docker.io/$DOCKER_REGISTRY"
 PDP_CLUSTER_NAME="pdp-cluster"
 
 if [ $TRAVIS_EVENT_TYPE == "push" ] && [ $TRAVIS_BRANCH == "master" ]; then
-  echo "Start deploy proccess"
+  echo "Start deploy proccess for $target"
   
   pip install --user awscli
   export PATH=$PATH:$HOME/.local/bin
 
   source ./devops/scripts/get-docker-repo-name.sh $target
-  TASK_NAME=$DOCKER_REPO_NAME
+  DEPLOY_SERVICE=$DOCKER_REPO_NAME
 
-  ./devops/scripts/ecs.sh -c $PDP_CLUSTER_NAME -d $TASK_NAME -i "$DOCKER_REGISTRY_URL/$DOCKER_REPO_NAME:latest" -r $AWS_REGION -t 240
+  ./devops/scripts/ecs.sh -c $PDP_CLUSTER_NAME -n $DEPLOY_SERVICE "$DOCKER_REGISTRY_URL/$DOCKER_REPO_NAME:latest" -r $AWS_REGION -t 240
 else
   echo "Skip deploy"
 fi
