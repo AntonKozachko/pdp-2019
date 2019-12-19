5fbdf64591cacba644a241382228e9c3ae466301
Anton Kozachko <anton_ko@hotmail.com>
2019-12-19 23:22:22 +0200

    remove axios timeout

--- web-app/app/containers/AuthProvider/use-auth.js
+++ web-app/app/containers/AuthProvider/use-auth.js
@@ -26,10 +26,7 @@ function useProvideAuth() {
   const authHost = process.env.AUTH_API_HOST;
   const authPort = process.env.AUTH_PORT;
 
-  const authInstance = axios.create({
-    baseURL: `http://${authHost}:${authPort}/user`,
-    timeout: 10000,
-  });
+  const authInstance = axios.create({ baseURL: `http://${authHost}:${authPort}/user` });
 
   const login = async credentials => {
     dispatch({ type: actions.request });
